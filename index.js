16315b08bc88940d000defe54ad1f95b48948de4
Chester Bernabe <bernabealan05@gmail.com>
2024-02-22 20:20:08 +0800

    second commit

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const app = express();
 const port = 3000;
 const API_URl = "https://www.dnd5eapi.co/api/";
 var classChosen = "";
-
+var skillsChosen = [];
+var listOfSkills=[];
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -33,8 +34,19 @@ res.render("index.ejs",{chosenClass: classChosen})
 app.post("/get-skills", async(req,res)=>{
     try {
         const result = await axios.get(API_URl + "spells");
-        console.log(result.data.results);
-        res.render("index.ejs",{listSkills: result.data.results});
+        listOfSkills = result.data.results;
+        res.render("index.ejs",{listSkills: result.data.results, chosenClass: classChosen});
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.post("/chosen-skills", (req,res)=>{
+    try {
+        if(skillsChosen.length < 5){
+        skillsChosen.push(req.body.skill)
+        }
+        res.render("index.ejs",{chosenSkills: skillsChosen, listSkills: listOfSkills, chosenClass: classChosen});
     } catch (error) {
         console.log(error);
     }
